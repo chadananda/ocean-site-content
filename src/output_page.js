78a8caa9e907dafa84fe79f16a88874399f5c162
David Hunt <git@dnotes.net>
2018-04-15 21:51:55 -0700

    added console logging to output_page

--- src/output_page.js
+++ src/output_page.js
@@ -1,17 +1,15 @@
-const fs = require('fs') 
+const fs = require('fs')
 const mkdirp = require('mkdirp')
 
 
-module.exports = function(outputFolder, outputFile, meta, markdown) { 
+module.exports = function(outputFolder, outputFile, meta, markdown) {
   var header = [].concat(['---'], Object.keys(meta).map((key) =>`${key}: ${meta[key]}`), ['---']).join('\n')
-  //console.log(`Outputing file: ${outputFile}`) 
-  
   // output with forced directory
   mkdirp(outputFolder, function (err) {
     if (err) console.error(err)
-    else fs.writeFileSync(outputFile, header+'\n\n\n'+markdown, 'UTF-8') 
+    else {
+      fs.writeFileSync(outputFile, header+'\n\n\n'+markdown, 'UTF-8')
+      console.log('Saved file:  ' + outputFolder + outputFile)
+    }
   })
 }
-  
-  
-  
