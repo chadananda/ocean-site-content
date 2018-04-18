5d5f2aa26b9580c3b1c1faea7813fa48a0b18da2
David Hunt <git@dnotes.net>
2018-04-17 18:58:59 -0700

    Fixed arrays in meta info output to yaml front matter

--- src/output_page.js
+++ src/output_page.js
@@ -3,7 +3,14 @@ const mkdirp = require('mkdirp')
 
 
 module.exports = function(outputFolder, outputFile, meta, markdown) {
-  var header = [].concat(['---'], Object.keys(meta).map((key) =>`${key}: ${meta[key]}`), ['---']).join('\n')
+  var header = [].concat(['---'], Object.keys(meta).map((key) => {
+    if (typeof(meta[key]) === 'array' || typeof(meta[key]) === 'object') {
+      return (meta[key].length > 1 ? [key + ':', ...meta[key].toArray()].join('\n  - ') : `${key}: ${meta[key][0]}`)
+    }
+    else {
+      return `${key}: ${meta[key]}`
+    }
+  }), ['---']).join('\n')
   // output with forced directory
   mkdirp(outputFolder, function (err) {
     if (err) console.error(err)
