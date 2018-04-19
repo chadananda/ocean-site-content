568f282a5f20b748298cfad8d3dade089e40e130
David Hunt <git@dnotes.net>
2018-04-18 21:20:18 -0700

    updated filecount script to include index files

--- filecount.sh
+++ filecount.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Collection	Short	Scraped	Crawled" > ./output/filecount.tsv
 for d in output/*/ ; do
-	echo "${d:7}	$(find $d -type f -name '.*.md' | wc -l)	$(ls -l $d | wc -l)	$([[ -f $d/.index.txt ]] && cat $d/.index.txt | wc -l)" >> ./output/filecount.tsv;
+	echo "${d:7}	$(find $d -type f -name '.*.md' | wc -l | xargs)	$(ls -l $d | wc -l | xargs)	$([[ -f $d/.index.txt ]] && cat $d/.index.txt | wc -l | xargs)" >> ./output/filecount.tsv;
 done
